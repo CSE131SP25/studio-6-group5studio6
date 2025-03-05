0c629a0b41322607dc1ee6647100b404a1467de0
gpurd <gpurd@DESKTOP-3L2STKN.dhcp.wustl.edu>
2025-03-05 15:47:15 -0600

    1

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,9 +12,13 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+			double sum=0.0;
+			sum = sum + Math.pow(0.5, n);
+			if(n < 1) {
+				return 0.0;
+			}else {
+				return sum + geometricSum(n-1);
+			}
 		
 	}
 
@@ -29,8 +33,13 @@ public class RecursiveMethods {
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius, double radiusMinimumDrawingThreshold) {
-		
-		// FIXME complete the recursive drawing
+		if(radius>radiusMinimumDrawingThreshold) {
+			StdDraw.circle(xCenter, yCenter, radius);
+			circlesUponCircles(xCenter+radius,yCenter,radius/3,radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter-radius,yCenter,radius/3,radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter,yCenter-radius,radius/3,radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter,yCenter+radius,radius/3,radiusMinimumDrawingThreshold);
+		}
 	}
 	
 
@@ -41,10 +50,24 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
+		int[] x = new int[array.length];
+		int index = 0;
+		int lastIndex = array.length - 1;
+		if (index > lastIndex) {
+			return x;
+		} else {
+			return toReversedHelper(x,index+1,array,lastIndex);
+		}
 		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+	}
+	
+	public static int[] toReversedHelper(int[] arrayReversed, int indexHelper, int[]array, int lastIndex) {
 		
+		int mirrorIndex = lastIndex - indexHelper;
+		arrayReversed[indexHelper] = array[mirrorIndex];
+		arrayReversed[mirrorIndex] = array[indexHelper];
+		
+		return arrayReversed;
 	}
 
 	/**
